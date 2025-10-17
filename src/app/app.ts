29b60a2e8c34542744593a80001004080ac6864a
diegosorianom <156294535+diegosorianom@users.noreply.github.com>
2025-10-17 21:55:16 +0200

    Ng-Icons installation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { phosphorAcorn } from '@ng-icons/phosphor-icons/regular'
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIcon],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  viewProviders: [provideIcons({ phosphorAcorn})]
 })
 export class App {
   protected readonly title = signal('tmdb-app-angular');
