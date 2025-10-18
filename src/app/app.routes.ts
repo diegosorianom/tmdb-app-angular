9a1783d08753236300f62945aa8796b55083b998
diego <diegosorianomagallon@gmail.com>
2025-10-18 17:52:46 +0200

    Search-Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
+import { SearchPage } from './pages/search-page/search-page';
 
 export const routes: Routes = [
-    { path: '', component: HomePage}
+    { path: '', component: HomePage},
+    { path: 'search', component: SearchPage}
 ];
--- src/app/components/movie-grid/movie-grid.ts
+++ src/app/components/movie-grid/movie-grid.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from "@ng-icons/core";
 import { phosphorFilmSlateFill } from '@ng-icons/phosphor-icons/fill';
 import { phosphorPlus } from '@ng-icons/phosphor-icons/regular';
+import { RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-movie-grid',
-  imports: [NgIcon],
+  imports: [NgIcon, RouterLink],
   templateUrl: './movie-grid.html',
   styleUrl: './movie-grid.css',
   viewProviders: [provideIcons({ phosphorPlus, phosphorFilmSlateFill })]
